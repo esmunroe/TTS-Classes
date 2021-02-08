26708614cd5031f5374db2564bcad5dbea6e0897
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-08 09:13:24 -0500

    Student class constructors

--- src/classes/Student.java
+++ src/classes/Student.java
@@ -7,4 +7,19 @@ public class Student {
     private int age;
     private String gradeLevel;
     private ArrayList<String> classes;
+
+    public Student() {}
+
+    public Student(String name, int age, String gradeLevel) {
+        this.name = name;
+        this.age = age;
+        this.gradeLevel = gradeLevel;
+    }
+
+    public Student(String name, int age, String gradeLevel, ArrayList<String> classes) {
+        this.name = name;
+        this.age = age;
+        this.gradeLevel = gradeLevel;
+        this.classes = classes;
+    }
 }
