125f1fc8f0c1df79516ffb216c9fd810de964dac
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-08 09:09:11 -0500

    Pet class get and set methods

--- src/classes/Pet.java
+++ src/classes/Pet.java
@@ -14,4 +14,28 @@ public class Pet {
         this.location = location;
         this.type = type;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
