4b09af0b4a8ca419bff6943292d21a2ed9b68d02
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-08 09:12:14 -0500

    Student class instance variables

--- src/classes/Student.java
+++ src/classes/Student.java
@@ -1,4 +1,10 @@
 package classes;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String name;
+    private int age;
+    private String gradeLevel;
+    private ArrayList<String> classes;
 }
