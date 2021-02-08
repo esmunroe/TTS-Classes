b65d7b10b73cc5a3d2da8926878763b48c1f6126
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-08 09:07:48 -0500

    Pet class constructors

--- src/classes/Pet.java
+++ src/classes/Pet.java
@@ -5,4 +5,13 @@ public class Pet {
     private int age;
     private String location;
     private String type;
+
+    public Pet() {}
+
+    public Pet(String name, int age, String location, String type) {
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.type = type;
+    }
 }
